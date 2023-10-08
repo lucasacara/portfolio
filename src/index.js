67b59a0df19c983f0b72e2ed5d8dd0146b1e8c3d
Lucas Jandrey Leismann <57455862+lucasacara@users.noreply.github.com>
2023-10-08 13:18:32 -0300

    Correcting bugs

--- src/index.js
+++ src/index.js
@@ -15,3 +15,9 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
